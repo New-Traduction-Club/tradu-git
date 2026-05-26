5750f06f3231f851f4dc4b91b2f4196884b1d4e2
Just3090 <justnitroso77@outlook.com>
2026-05-26 12:14:01 -0600

    edit namespace

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,7 +14,7 @@ if (localPropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.tradu_git"
+    namespace = "com.tdclub.tradu_git"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -25,7 +25,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.tradu_git"
+        applicationId = "com.tdclub.tradu_git"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/tradu_git/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/tradu_git/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.tradu_git
+package com.tdclub.tradu_git
 
 import io.flutter.embedding.android.FlutterActivity
 
